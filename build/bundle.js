ef2eca55a0496b5e0f96dc1a2ef27aeb64868901
Alive Kuo <alegnadise@gmail.com>
2016-05-13 18:35:41 +0800

    try fix img path

--- build/bundle.js
+++ build/bundle.js
@@ -9925,7 +9925,7 @@
 	});
 	var MovieData = [{
 	   "name": " Money Monster (2016)",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BMTgzMjU1NDUwMl5BMl5BanBnXkFtZTgwODg1NjY0ODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BMTgzMjU1NDUwMl5BMl5BanBnXkFtZTgwODg1NjY0ODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
 	   "outline": "Financial TV host Lee Gates and his producer Patty are put in an extreme situation when an irate investor takes over their studio.",
 	   "duration": "PT98M",
 	   "director": "Jodie Foster",
@@ -9934,7 +9934,7 @@
 	   "id": "tt2241351"
 	}, {
 	   "name": " The Darkness (2016)",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BMjIxMTg2NjAyMV5BMl5BanBnXkFtZTgwNDE5ODIxODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BMjIxMTg2NjAyMV5BMl5BanBnXkFtZTgwNDE5ODIxODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
 	   "outline": "A family returns from a Grand Canyon vacation, haunted by an ancient supernatural entity they unknowingly awakened and engages them in a fight for their survival.",
 	   "duration": "PT92M",
 	   "director": "Greg McLean",
@@ -9943,7 +9943,7 @@
 	   "id": "tt1878841"
 	}, {
 	   "name": " Love & Friendship (2016) - [Limited]",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BMTQ3NTQ2NjMwMV5BMl5BanBnXkFtZTgwOTk3Njk0ODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BMTQ3NTQ2NjMwMV5BMl5BanBnXkFtZTgwOTk3Njk0ODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
 	   "outline": "Lady Susan Vernon takes up temporary residence at her in-laws' estate and, while there, is determined to be a matchmaker for her daughter Frederica -- and herself too, naturally.",
 	   "duration": "PT92M",
 	   "director": "Whit Stillman",
@@ -9952,7 +9952,7 @@
 	   "id": "tt3068194"
 	}, {
 	   "name": " The Lobster (2015) - [Limited]",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BNDQ1NDE5NzQ1NF5BMl5BanBnXkFtZTgwNzA5OTM2NTE@._V1_UY209_CR3,0,140,209_AL_.jpg",
+	   "img": "images/MV5BNDQ1NDE5NzQ1NF5BMl5BanBnXkFtZTgwNzA5OTM2NTE@._V1_UY209_CR3,0,140,209_AL_.jpg",
 	   "outline": "In a dystopian near future, single people, according to the laws of The City, are taken to The Hotel, where they are obliged to find a romantic partner in forty-five days or are transformed into beasts and sent off into The Woods.",
 	   "duration": "PT118M",
 	   "director": "Yorgos Lanthimos",
@@ -9961,7 +9961,7 @@
 	   "id": "tt3464902"
 	}, {
 	   "name": " High-Rise (2015) - [Limited]",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BMjM3MjM4NzMwMl5BMl5BanBnXkFtZTgwMTQ4MjUzODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BMjM3MjM4NzMwMl5BMl5BanBnXkFtZTgwMTQ4MjUzODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
 	   "outline": "Life for the residents of a tower block begins to run out of control.",
 	   "duration": "PT119M",
 	   "director": "Ben Wheatley",
@@ -9970,7 +9970,7 @@
 	   "id": "tt0462335"
 	}, {
 	   "name": " Last Days in the Desert (2015) - [Limited]",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BMTQ2NDk0MTc1MV5BMl5BanBnXkFtZTgwMTU3MjM0ODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BMTQ2NDk0MTc1MV5BMl5BanBnXkFtZTgwMTU3MjM0ODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
 	   "outline": "An imagined chapter from Jesus' forty days of fasting and praying in the desert. On his way out of the wilderness, Jesus struggles with the Devil over the fate of a family in crisis.",
 	   "duration": "PT98M",
 	   "director": "Rodrigo García",
@@ -9979,7 +9979,7 @@
 	   "id": "tt3513054"
 	}, {
 	   "name": " Search Party (2014) - [Limited]",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BNDM3MjE4ODQzMV5BMl5BanBnXkFtZTgwMDY0NDgzODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BNDM3MjE4ODQzMV5BMl5BanBnXkFtZTgwMDY0NDgzODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
 	   "outline": "A pair of friends embark on a mission to reunite their pal with the woman he was going to marry.",
 	   "duration": "PT93M",
 	   "director": "Scot Armstrong",
@@ -9988,7 +9988,7 @@
 	   "id": "tt2758904"
 	}, {
 	   "name": " Dheepan (2015) - [Limited]",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BMzc1NzgwMzgyM15BMl5BanBnXkFtZTgwMzAwMzQ1ODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BMzc1NzgwMzgyM15BMl5BanBnXkFtZTgwMzAwMzQ1ODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
 	   "outline": "Dheepan is a Sri Lankan Tamil warrior who flees to France and ends up working as a caretaker outside Paris.",
 	   "duration": "PT115M",
 	   "director": "Jacques Audiard",
@@ -9997,7 +9997,7 @@
 	   "id": "tt4082068"
 	}, {
 	   "name": " Saat po long 2 (2015) - [Limited]",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BMTkzNzk2MDI0MF5BMl5BanBnXkFtZTgwNTE1MTM1ODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BMTkzNzk2MDI0MF5BMl5BanBnXkFtZTgwNTE1MTM1ODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
 	   "outline": "Undercover cop Kit (Wu Jing) becomes a junkie in order to catch Mr Hung (Louis Koo), the mastermind behind a crime syndicate. When the operation goes sour and Kit blows his cover, his supervisor and uncle Wah (Simon Yam) decides to terminate the operation. When Kit disappears without a trace, Wah defies the order from his commanding officer and tracks Kit to a prison in Thailand. Thai Police officer Chai (Tony Jaa) becomes a prison guard in order to raise money for his daughter who has leukemia. He's assigned to keep an eye on Kit. Even though Chai and Kit are in opposing positions and they don't speak a common language, Kit turns out to be a suitable bone marrow donor who can save Chai's daughter. While Chai is determined to keep Kit alive, the warden Ko (Zhang Jin) wants him dead to ensure the smooth operation of the prison, which is the front for Mr Hung's organ trafficking business. Mr Hung shows up in Thailand so he can use his younger brother's (Jun Kung) heart in a heart transplant to save his own life. The stage is set for a showdown...",
 	   "duration": "PT120M",
 	   "director": "Pou-Soi Cheang",
@@ -10006,7 +10006,7 @@
 	   "id": "tt3197766"
 	}, {
 	   "name": " Sunset Song (2015) - [Limited]",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BMTAzOTEwNzAyNTJeQTJeQWpwZ15BbWU4MDgyNzE2OTcx._V1_UY209_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BMTAzOTEwNzAyNTJeQTJeQWpwZ15BbWU4MDgyNzE2OTcx._V1_UY209_CR0,0,140,209_AL_.jpg",
 	   "outline": "The daughter of a Scottish farmer comes of age in the early 1900s.",
 	   "duration": "PT135M",
 	   "director": "Terence Davies",
@@ -10015,7 +10015,7 @@
 	   "id": "tt2262161"
 	}, {
 	   "name": " Sorgenfri (2015) - [Limited]",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BNTRmZjZhNTMtMTllMC00ZmRjLWJlZTEtZjFhZWIxYzQ1ZjNlXkEyXkFqcGdeQXVyMjIzMDczOTA@._V1_UY209_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BNTRmZjZhNTMtMTllMC00ZmRjLWJlZTEtZjFhZWIxYzQ1ZjNlXkEyXkFqcGdeQXVyMjIzMDczOTA@._V1_UY209_CR0,0,140,209_AL_.jpg",
 	   "outline": "A family of four is quarantined in their home as a virulent strand of the flu spreads into town and they are forced to the extreme to escape alive.",
 	   "duration": "PT85M",
 	   "director": "Bo Mikkelsen",
@@ -10024,7 +10024,7 @@
 	   "id": "tt3547682"
 	}, {
 	   "name": " Captain America: Civil War (2016)",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
 	   "outline": "Political interference in the Avengers' activities causes a rift between former allies Captain America and Iron Man.",
 	   "duration": "PT147M",
 	   "director": "Anthony Russo",
@@ -10033,7 +10033,7 @@
 	   "id": "tt3498820"
 	}, {
 	   "name": " The Jungle Book (2016)",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BMTc3NTUzNTI4MV5BMl5BanBnXkFtZTgwNjU0NjU5NzE@._V1_UY209_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BMTc3NTUzNTI4MV5BMl5BanBnXkFtZTgwNjU0NjU5NzE@._V1_UY209_CR0,0,140,209_AL_.jpg",
 	   "outline": "After a threat from the tiger, Sheer Khan, forces him to flee the jungle, a man-cub named, Mowgli, embarks on a journey of self discovery with the aid of the panther, Bagheera, and the free spirited bear, Baloo.",
 	   "duration": "PT106M",
 	   "director": "Jon Favreau",
@@ -10042,7 +10042,7 @@
 	   "id": "tt3040964"
 	}, {
 	   "name": " Mother's Day (2016)",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BODYzMjc5NDEzNF5BMl5BanBnXkFtZTgwMzI3ODcyODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BODYzMjc5NDEzNF5BMl5BanBnXkFtZTgwMzI3ODcyODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
 	   "outline": "Three generations come together in the week leading up to Mother's Day.",
 	   "duration": "PT118M",
 	   "director": "Garry Marshall",
@@ -10051,7 +10051,7 @@
 	   "id": "tt4824302"
 	}, {
 	   "name": " The Huntsman: Winter's War (2016)",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BMTkzNzc2MDQ1N15BMl5BanBnXkFtZTgwNTAyNzkyNzE@._V1_UX140_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BMTkzNzc2MDQ1N15BMl5BanBnXkFtZTgwNTAyNzkyNzE@._V1_UX140_CR0,0,140,209_AL_.jpg",
 	   "outline": "As a war between rival queen sisters Ravenna and Freya escalates, Eric and fellow warrior Sara, members of the Huntsmen army raised to protect Freya, try to conceal their forbidden love as they combat Ravenna's wicked intentions.",
 	   "duration": "PT114M",
 	   "director": "Cedric Nicolas-Troyan",
@@ -10060,7 +10060,7 @@
 	   "id": "tt2381991"
 	}, {
 	   "name": " Keanu (2016)",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BMTUwODA0NzQxMl5BMl5BanBnXkFtZTgwNzUyMjY3ODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BMTUwODA0NzQxMl5BMl5BanBnXkFtZTgwNzUyMjY3ODE@._V1_UY209_CR0,0,140,209_AL_.jpg",
 	   "outline": "Friends hatch a plot to retrieve a stolen kitten by posing as drug dealers for a street gang.",
 	   "duration": "PT100M",
 	   "director": "Peter Atencio",
@@ -10069,7 +10069,7 @@
 	   "id": "tt4139124"
 	}, {
 	   "name": " Barbershop: The Next Cut (2016)",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BMzUxMzM5MjIxNF5BMl5BanBnXkFtZTgwMDk1NzMzNzE@._V1_UY209_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BMzUxMzM5MjIxNF5BMl5BanBnXkFtZTgwMDk1NzMzNzE@._V1_UY209_CR0,0,140,209_AL_.jpg",
 	   "outline": "As their surrounding community has taken a turn for the worse, the crew at Calvin's Barbershop come together to bring some much needed change to their neighborhood.",
 	   "duration": "PT111M",
 	   "director": "Malcolm D. Lee",
@@ -10078,7 +10078,7 @@
 	   "id": "tt3628584"
 	}, {
 	   "name": " Zootopia (2016)",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BOTMyMjEyNzIzMV5BMl5BanBnXkFtZTgwNzIyNjU0NzE@._V1_UY209_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BOTMyMjEyNzIzMV5BMl5BanBnXkFtZTgwNzIyNjU0NzE@._V1_UY209_CR0,0,140,209_AL_.jpg",
 	   "outline": "In a city of anthropomorphic animals, a rookie bunny cop and a cynical con artist fox must work together to uncover a conspiracy.",
 	   "duration": "PT108M",
 	   "director": "Byron Howard",
@@ -10087,7 +10087,7 @@
 	   "id": "tt2948356"
 	}, {
 	   "name": " The Boss (2016)",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BMjQzNTUwNDU0OV5BMl5BanBnXkFtZTgwNzU5NTAwODE@._V1_UX140_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BMjQzNTUwNDU0OV5BMl5BanBnXkFtZTgwNzU5NTAwODE@._V1_UX140_CR0,0,140,209_AL_.jpg",
 	   "outline": "A titan of industry is sent to prison after she's caught insider trading. When she emerges ready to rebrand herself as America's latest sweetheart, not everyone she screwed over is so quick to forgive and forget.",
 	   "duration": "PT99M",
 	   "director": "Ben Falcone",
@@ -10096,7 +10096,7 @@
 	   "id": "tt2702724"
 	}, {
 	   "name": " Ratchet & Clank (2016)",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BNDMxMjkzOTEzOV5BMl5BanBnXkFtZTgwNDExNjk5NzE@._V1_UY209_CR0,0,140,209_AL_.jpg",
+	   "img": "images/MV5BNDMxMjkzOTEzOV5BMl5BanBnXkFtZTgwNDExNjk5NzE@._V1_UY209_CR0,0,140,209_AL_.jpg",
 	   "outline": "When the galaxy comes under the threat of a nefarious space captain, a mechanic and his newfound robot ally join an elite squad of combatants to save the universe.",
 	   "duration": "PT94M",
 	   "director": "Kevin Munroe",
@@ -10105,7 +10105,7 @@
 	   "id": "tt2865120"
 	}, {
 	   "name": " Batman v Superman: Dawn of Justice (2016)",
-	   "img": "http://ia.media-imdb.com/images/M/MV5BNTE5NzU3MTYzOF5BMl5BanBnXkFtZTgwNTM5NjQxODE@._V1_UY209_CR1,0,140,209_AL_.jpg",
+	   "img": "images/MV5BNTE5NzU3MTYzOF5BMl5BanBnXkFtZTgwNTM5NjQxODE@._V1_UY209_CR1,0,140,209_AL_.jpg",
 	   "outline": "Fearing that the actions of Superman are left unchecked, Batman takes on the Man of Steel, while the world wrestles with what kind of a hero it really needs.",
 	   "duration": "PT151M",
 	   "director": "Zack Snyder",
